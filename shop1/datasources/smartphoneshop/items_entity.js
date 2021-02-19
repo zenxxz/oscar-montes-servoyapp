e775a54836c530d92ea93a55716651c3dcf7cde5
zenxxz <46887053+zenxxz@users.noreply.github.com>
2021-02-19 17:39:38 +0800

    Add files via upload

---
+++ shop1/datasources/smartphoneshop/items_entity.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param oldValue
+ * @param newValue
+ * @param {JSEvent} event
+ *
+ * @return {boolean}
+ *
+ * @properties={typeid:24,uuid:"74A0A8E0-3574-4784-9B94-810DE21B60A1"}
+ */
+function addItemName(oldValue, newValue, event) {
+	// TODO Auto-generated method stub
+	return true
+}
+
+/**
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"083BAFD0-802A-4A11-9085-3EC38B472028"}
+ */
+function onAction(event) {
+	// TODO Auto-generated method stub
+
+}
